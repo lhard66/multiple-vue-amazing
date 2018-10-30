245dc676a86a4f5711902dc41f2d500164834c31
Allen Liu <lhard@qq.com>
2018-10-30 16:32:34 +0800

    优化删除iview和element-ui

---
+++ src/core/views/home/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <span>core -> home</span>
+</template>
--- src/login/login.js
+++ src/login/login.js
@@ -2,11 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Login from './login.vue'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
-Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
